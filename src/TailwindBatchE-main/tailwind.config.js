c2e6ddbd9b693b7dd4710ad4c221f3918d938d2a
Tshrinetra <122108843+Tshrinetra@users.noreply.github.com>
2025-04-10 08:28:10 +0530

    abhi

---
+++ src/TailwindBatchE-main/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    
+    extend: {
+      container:{
+        padding:'3rem',
+        center:true
+      }
+    },
+  },
+  plugins: [],
+}
+
